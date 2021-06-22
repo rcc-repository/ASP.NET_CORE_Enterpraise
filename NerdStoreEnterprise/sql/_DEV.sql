105bb3440dd1c33afeb253334a8e18b3bc1e622c
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-06-22 11:54:24 -0300

    Codando

---
+++ NerdStoreEnterprise/sql/_DEV.sql
@@ -0,0 +1,19 @@
+
+--******************************
+--Identity
+
+select * from AspNetUsers
+select * from AspNetUserClaims
+
+--INSERT INTO AspNetUserClaims VALUES (NULL, 'e753877f-4e47-42f8-831f-d321aff89873', '', '')
+
+select * from AspNetUserLogins
+
+select * from AspNetRoles
+select * from AspNetRoleClaims
+select * from AspNetUserRoles
+
+select * from AspNetUserTokens
+
+--******************************
+
