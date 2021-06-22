387304a29622f3d7228becdbd678f92a0b131026
Rodrigo Croci Costa <rodrigo.croci@gmail.com>
2021-06-22 16:28:58 -0300

    Codando

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -2,18 +2,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NSE.Identidade.API.Configuration;
 
 namespace NSE.Identidade.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -29,7 +41,6 @@ namespace NSE.Identidade.API
             app.UseSwaggerConfiguration();
 
             app.UseApiConfiguration(env);
-
         }
     }
 }
